28d14ea73f0718f5ce99b4fbd6320da8e61cd50d
wnstn <84610692+Focus150@users.noreply.github.com>
2021-08-23 03:43:21 +0900

    Delete src directory

--- src/main/java/com/study/codemoa/board/exception/BoardException.java
+++
@@ -1,6 +0,0 @@
-package com.study.codemoa.board.exception;
-
-public class BoardException extends RuntimeException {
-	public BoardException() {}
-	public BoardException(String msg) {super(msg);}
-}
