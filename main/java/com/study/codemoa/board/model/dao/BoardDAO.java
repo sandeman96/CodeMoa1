5c37c76fad86431c35de8ee479e768ad6fd770f2
DESKTOP-TVP9GOC\jhw56 <jhw5647@naver.com>
2021-08-31 15:41:40 +0900

    댓글까지 수정 완료 / pagination 수정 완료 / 정렬 기준 완료 / 보드 전체 코드 정리 0831

---
+++ main/java/com/study/codemoa/board/model/dao/BoardDAO.java
@@ -0,0 +1,77 @@
+package com.study.codemoa.board.model.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.ibatis.session.RowBounds;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.study.codemoa.board.model.vo.Board;
+import com.study.codemoa.board.model.vo.Likey;
+import com.study.codemoa.board.model.vo.PageInfo;
+import com.study.codemoa.board.model.vo.Reply;
+
+@Repository("bDAO")
+public class BoardDAO {
+
+	public int getListCount(SqlSessionTemplate sqlSession, int bType) {
+		return sqlSession.selectOne("boardMapper.getListCount", bType);
+		
+	}
+
+	public ArrayList<Board> selectList(SqlSessionTemplate sqlSession, PageInfo pi, HashMap<String, String> map) {
+		int offset = pi.getBoardLimit() * (pi.getCurrentPage()-1);
+		RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
+		return (ArrayList) sqlSession.selectList("boardMapper.selectList", map, rowBounds);
+	}
+	
+	public int insertBoard(SqlSessionTemplate sqlSession, Board b) {
+		return sqlSession.insert("boardMapper.insertBoard", b);
+	}
+
+	public int addCount(SqlSessionTemplate sqlSession, int bNo) {
+		return sqlSession.update("boardMapper.addCount", bNo);
+	}
+
+	public Board selectBoard(SqlSessionTemplate sqlSession, int bNo) {
+		return sqlSession.selectOne("boardMapper.selectBoard", bNo);
+	}
+
+	public int updateBoard(SqlSessionTemplate sqlSession, Board b) {
+		return sqlSession.update("boardMapper.updateBoard", b);
+	}
+
+	public int deleteBoard(SqlSessionTemplate sqlSession, int bNo) {
+		return sqlSession.update("boardMapper.deleteBoard", bNo);
+	}
+
+	public Likey selectLike(SqlSessionTemplate sqlSession, Likey likey) {
+		return sqlSession.selectOne("boardMapper.selectLike", likey);
+	}
+
+	public int insertLike(SqlSessionTemplate sqlSession, Likey likey) {
+		return sqlSession.insert("boardMapper.insertLike", likey);
+	}
+	
+	public int deleteLike(SqlSessionTemplate sqlSession, Likey likey) {
+		return sqlSession.delete("boardMapper.deleteLike", likey);
+	}
+
+	public int insertReply(SqlSessionTemplate sqlSession, Reply r) {
+		return sqlSession.insert("boardMapper.insertReply", r);
+	}
+
+	public ArrayList<Reply> selectReplyList(SqlSessionTemplate sqlSession, int bNo) {
+		return (ArrayList) sqlSession.selectList("boardMapper.selectReplyList", bNo);
+	}
+
+	public int updateReply(SqlSessionTemplate sqlSession, Reply r) {
+		return sqlSession.update("boardMapper.updateReply", r);
+	}
+
+	public int deleteReply(SqlSessionTemplate sqlSession, int rNo) {
+		return sqlSession.update("boardMapper.deleteReply", rNo);
+	}
+
+}
